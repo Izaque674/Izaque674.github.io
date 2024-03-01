cbb865f173af59b1d576231c429d1430ba097755
Izaque674 <115513122+Izaque674@users.noreply.github.com>
2024-02-29 22:07:14 -0300

    Delete chamadaReal directory

--- chamadaReal/chamadoReal/valida_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-require_once'../chamadoReal/src/controller/loginController.php';
-$loginController = new LoginController();
-$loginController ->auntenticar($_POST['email'], $_POST['senha']);
-
-?>
